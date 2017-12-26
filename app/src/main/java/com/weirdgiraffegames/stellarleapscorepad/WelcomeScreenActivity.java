68e4972d3a26cf3d3ab1bf36375f1ce1dad6d0ba
fullmers <sarah.fullmer@gmail.com>
2017-12-26 14:57:42 +0100

    Create AboutActivity, make layout skeleton, link button from welcome screen to sbout acitivuty

--- app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/WelcomeScreenActivity.java
+++ app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/WelcomeScreenActivity.java
@@ -1,13 +1,27 @@
 package com.weirdgiraffegames.stellarleapscorepad;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
+
+    Button aboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        aboutButton = findViewById(R.id.about_app_btn);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(WelcomeScreenActivity.this,AboutActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
