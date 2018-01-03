7536f81b0c624caf3a83d228cdb373fa41dc89bf
fullmers <sarah.fullmer@gmail.com>
2018-01-03 12:20:27 +0100

    create query to get all gamelogs from db

--- app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/GameLogActivity.java
+++ app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/GameLogActivity.java
@@ -1,13 +1,38 @@
 package com.weirdgiraffegames.stellarleapscorepad;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.weirdgiraffegames.stellarleapscorepad.data.GameLogContract;
+import com.weirdgiraffegames.stellarleapscorepad.data.GameLogDbHelper;
 
 public class GameLogActivity extends AppCompatActivity {
+
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_log);
+
+        GameLogDbHelper dbHelper = new GameLogDbHelper(this);
+
+        mDb = dbHelper.getWritableDatabase();
+    }
+
+
+    public Cursor getAllGameLogs() {
+
+        return mDb.query(GameLogContract.GameLogEntry.TABLE_NAME,
+                null, //interested in all columns, so just pass in null
+                null,
+                null,
+                null,
+                null,
+                GameLogContract.GameLogEntry.COLUMN_TIMESTAMP //order results by timestamp
+        );
+
     }
 }
