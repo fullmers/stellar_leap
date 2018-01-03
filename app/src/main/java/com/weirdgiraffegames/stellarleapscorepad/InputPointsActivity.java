88dfd988cf52cc34dd8a282c5aedcc774375c273
fullmers <sarah.fullmer@gmail.com>
2018-01-03 21:07:07 +0100

    define all input point edit texts

--- app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/InputPointsActivity.java
+++ app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/InputPointsActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -14,17 +15,43 @@ import butterknife.ButterKnife;
 
 public class InputPointsActivity extends AppCompatActivity {
 
-    ArrayList<String> selectedSpecies;
-    int numSelectedSpecies = -1;
-    int layoutIndex = 0;
+    private ArrayList<String> selectedSpecies;
+    private int numSelectedSpecies = -1;
+    private int layoutIndex = 0;
 
     @BindView(R.id.next_btn) Button nextButton;
+
     @BindView(R.id.tuskadon_layout) View tuskadonLayout;
     @BindView(R.id.starlings_layout) View starlingsLayout;
     @BindView(R.id.cosmosaurus_layout) View cosmosaurusLayout;
     @BindView(R.id.scoutars_layout)  View scoutarsLayout;
     @BindView(R.id.araklith_layout) View araklithLayout;
 
+    @BindView(R.id.tuskadon_mission_points_et) EditText tuskadonMissionPointsET;
+    @BindView(R.id.tuskadon_player_board_points_et) EditText tuskadonPlayerBoardPointsET;
+    @BindView(R.id.tuskadon_trait_points_et) EditText tuskadonTraitPointsET;
+    @BindView(R.id.tuskadon_resource_points_et) EditText tuskadonResourcePointsET;
+
+    @BindView(R.id.starlings_mission_points_et) EditText starlingsMissionPointsET;
+    @BindView(R.id.starlings_player_board_points_et) EditText starlingsPlayerBoardPointsET;
+    @BindView(R.id.starlings_trait_points_et) EditText starlingsTraitPointsET;
+    @BindView(R.id.starlings_resource_points_et) EditText starlingsResourcePointsET;
+
+    @BindView(R.id.cosmosaurus_mission_points_et) EditText cosmosaurusMissionPointsET;
+    @BindView(R.id.cosmosaurus_player_board_points_et) EditText cosmosauruPslayerBoardPointsET;
+    @BindView(R.id.cosmosaurus_trait_points_et) EditText cosmosaurusTraitPointsET;
+    @BindView(R.id.cosmosaurus_resource_points_et) EditText cosmosaurusResourcePointsET;
+
+    @BindView(R.id.scoutars_mission_points_et) EditText scoutarsMissionPointsET;
+    @BindView(R.id.scoutars_player_board_points_et) EditText scoutarsPlayerBoardPointsET;
+    @BindView(R.id.scoutars_trait_points_et) EditText scoutarsTraitPointsET;
+    @BindView(R.id.scoutars_resource_points_et) EditText scoutarsResourcePointsET;
+
+    @BindView(R.id.araklith_mission_points_et) EditText araklithMissionPointsET;
+    @BindView(R.id.araklith_player_board_points_et) EditText araklithPlayerBoardPointsET;
+    @BindView(R.id.araklith_trait_points_et) EditText araklithTraitPointsET;
+    @BindView(R.id.araklith_resource_points_et) EditText araklithResourcePointsET;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
