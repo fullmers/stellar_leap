098d1ce3ee9579fd7e1faa38b5ca0aca380509f4
fullmers <sarah.fullmer@gmail.com>
2018-01-05 11:57:56 +0100

    start creating content provider class, shell of required methods

---
+++ app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/data/GameLogContentProvider.java
@@ -0,0 +1,61 @@
+package com.weirdgiraffegames.stellarleapscorepad.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by sarah on 05/01/2018.
+ */
+
+public class GameLogContentProvider extends ContentProvider {
+
+    private GameLogDbHelper mDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mDbHelper = new GameLogDbHelper(context);
+        return true;
+    }
+
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
