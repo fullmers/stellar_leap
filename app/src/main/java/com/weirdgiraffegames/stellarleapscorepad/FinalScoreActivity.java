ddc930c26efc4479c6df575e7b13b8e0e3fad09f
fullmers <sarah.fullmer@gmail.com>
2018-01-10 14:12:55 +0100

    load data from content provider into final score activity layout

--- app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/FinalScoreActivity.java
+++ app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/FinalScoreActivity.java
@@ -10,9 +10,16 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import com.weirdgiraffegames.stellarleapscorepad.data.GameLogContract;
+import com.weirdgiraffegames.stellarleapscorepad.data.GameLogContract.GameLogEntry;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -24,11 +31,71 @@ public class FinalScoreActivity extends AppCompatActivity implements LoaderManag
     private static final String TAG = FinalScoreActivity.class.getSimpleName();
     private static final int FINAL_GAME_LOADER_ID = 1;
 
+    private static final int MISSION_POINTS_INDEX = 0;
+    private static final int PLAYER_BOARD_POINTS_INDEX = 1;
+    private static final int TRAIT_POINTS_INDEX = 2;
+    private static final int RESOURCES_POINTS_INDEX = 3;
+    private static final int TOTAL_POINTS_INDEX = 4;
+
+    private static final int TUSKADON_INDEX = 0;
+    private static final int STARLING_INDEX = 1;
+    private static final int COSMOSAURUS_INDEX = 2;
+    private static final int SCOUTAR_INDEX = 3;
+    private static final int ARAKLITH_INDEX = 4;
+
+    @BindView(R.id.btn_new_game) Button button;
+
+    @BindView(R.id.tuskadon_column) LinearLayout tuskadonPointsLayout;
+    @BindView(R.id.tuskadon_header) ImageView tuskadonHeaderImage;
+    @BindView(R.id.tuskadon_end_line) ImageView tuskadonEndLineImage;
+    @BindView(R.id.tuskadon_mission_points_tv) TextView tuskadonMissionPointsTv;
+    @BindView(R.id.tuskadon_player_board_points_tv) TextView tuskadonPlayerBoardPointsTv;
+    @BindView(R.id.tuskadon_trait_points_tv) TextView tuskadonTraitPointsTv;
+    @BindView(R.id.tuskadon_resources_points_tv) TextView tuskadonResourcesPointsTv;
+    @BindView(R.id.tuskadon_total_points_tv) TextView tuskadonTotalPointsTv;
+
+    @BindView(R.id.starling_column) LinearLayout starlingPointsLayout;
+    @BindView(R.id.starling_header) ImageView starlingHeaderImage;
+    @BindView(R.id.starling_end_line) ImageView starlingEndLineImage;
+    @BindView(R.id.starling_mission_points_tv) TextView starlingMissionPointsTv;
+    @BindView(R.id.starling_player_board_points_tv) TextView starlingPlayerBoardPointsTv;
+    @BindView(R.id.starling_trait_points_tv) TextView starlingTraitPointsTv;
+    @BindView(R.id.starling_resources_points_tv) TextView starlingResourcesPointsTv;
+    @BindView(R.id.starling_total_points_tv) TextView starlingTotalPointsTv;
+
+    @BindView(R.id.cosmosaurus_column) LinearLayout cosmosaurusPointsLayout;
+    @BindView(R.id.cosmosaurus_header) ImageView cosmosaurusHeaderImage;
+    @BindView(R.id.cosmosaurus_end_line) ImageView cosmosaurusEndLineImage;
+    @BindView(R.id.cosmosaurus_mission_points_tv) TextView cosmosaurusMissionPointsTv;
+    @BindView(R.id.cosmosaurus_player_board_points_tv) TextView cosmosaurusPlayerBoardPointsTv;
+    @BindView(R.id.cosmosaurus_trait_points_tv) TextView cosmosaurusTraitPointsTv;
+    @BindView(R.id.cosmosaurus_resources_points_tv) TextView cosmosaurusResourcesPointsTv;
+    @BindView(R.id.cosmosaurus_total_points_tv) TextView cosmosaurusTotalPointsTv;
+
+    @BindView(R.id.scoutars_column) LinearLayout scoutarsPointsLayout;
+    @BindView(R.id.scoutars_header) ImageView scoutarsHeaderImage;
+    @BindView(R.id.scoutars_end_line) ImageView scoutarsEndLineImage;
+    @BindView(R.id.scoutars_mission_points_tv) TextView scoutarsMissionPointsTv;
+    @BindView(R.id.scoutars_player_board_points_tv) TextView scoutarsPlayerBoardPointsTv;
+    @BindView(R.id.scoutars_trait_points_tv) TextView scoutarsTraitPointsTv;
+    @BindView(R.id.scoutars_resources_points_tv) TextView scoutarsResourcesPointsTv;
+    @BindView(R.id.scoutars_total_points_tv) TextView scoutarsTotalPointsTv;
+
+    @BindView(R.id.araklith_column) LinearLayout araklithPointsLayout;
+    @BindView(R.id.araklith_header) ImageView araklithHeaderImage;
+    @BindView(R.id.araklith_mission_points_tv) TextView araklithMissionPointsTv;
+    @BindView(R.id.araklith_player_board_points_tv) TextView araklithPlayerBoardPointsTv;
+    @BindView(R.id.araklith_trait_points_tv) TextView araklithTraitPointsTv;
+    @BindView(R.id.araklith_resources_points_tv) TextView araklithResourcesPointsTv;
+    @BindView(R.id.araklith_total_points_tv) TextView araklithTotalPointsTv;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_score);
         ButterKnife.bind(this);
+
         mUri = getIntent().getData();
         comesFromGameLogActivity = getIntent().getBooleanExtra(getString(R.string.comes_from_game_log_activity_key),false);
         Log.d("uri","in FinalScoreActivity: " + mUri.toString());
@@ -83,10 +150,110 @@ public class FinalScoreActivity extends AppCompatActivity implements LoaderManag
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         //TODO replace below load all data into UI
         if(cursor != null) {
+            ArrayList<String> selectedSpecies = new ArrayList<>();
             mCursor = cursor;
             mCursor.moveToFirst();
-            int tuskadonTotal = mCursor.getInt(cursor.getColumnIndex(GameLogContract.GameLogEntry.COLUMN_TUSKADON_TOTAL_POINTS));
-            Log.d("tuskadon total",String.valueOf(tuskadonTotal));
+
+            LinearLayout[] pointsColumnLayouts =
+                    {tuskadonPointsLayout,
+                            starlingPointsLayout,
+                            cosmosaurusPointsLayout,
+                            scoutarsPointsLayout,
+                            araklithPointsLayout};
+
+            ImageView[] headerImages =
+                    {tuskadonHeaderImage,
+                            starlingHeaderImage,
+                            cosmosaurusHeaderImage,
+                            scoutarsHeaderImage,
+                            araklithHeaderImage};
+
+            ImageView[] endLineImages =
+                    {tuskadonEndLineImage,
+                            starlingEndLineImage,
+                            cosmosaurusEndLineImage,
+                            scoutarsEndLineImage};
+
+            TextView[][] pointsTextViews = {
+                    {tuskadonMissionPointsTv,tuskadonPlayerBoardPointsTv,tuskadonTraitPointsTv,tuskadonResourcesPointsTv,tuskadonTotalPointsTv},
+                    {starlingMissionPointsTv,starlingPlayerBoardPointsTv,starlingTraitPointsTv,starlingResourcesPointsTv,starlingTotalPointsTv},
+                    {cosmosaurusMissionPointsTv,cosmosaurusPlayerBoardPointsTv,cosmosaurusTraitPointsTv,cosmosaurusResourcesPointsTv,cosmosaurusTotalPointsTv},
+                    {scoutarsMissionPointsTv,scoutarsPlayerBoardPointsTv,scoutarsTraitPointsTv,scoutarsResourcesPointsTv,scoutarsTotalPointsTv},
+                    {araklithMissionPointsTv,araklithPlayerBoardPointsTv,araklithTraitPointsTv,araklithResourcesPointsTv,araklithTotalPointsTv}
+            };
+
+            int[][] columnIndices = {
+                    {cursor.getColumnIndex(GameLogEntry.COLUMN_TUSKADON_MISSION_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_TUSKADON_PLAYER_BOARD_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_TUSKADON_TRAIT_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_TUSKADON_RESOURCE_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_TUSKADON_TOTAL_POINTS)},
+                    {cursor.getColumnIndex(GameLogEntry.COLUMN_STARLING_MISSION_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_STARLING_PLAYER_BOARD_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_STARLING_TRAIT_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_STARLING_RESOURCE_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_STARLING_TOTAL_POINTS)},
+                    {cursor.getColumnIndex(GameLogEntry.COLUMN_COSMOSAURUS_MISSION_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_COSMOSAURUS_PLAYER_BOARD_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_COSMOSAURUS_TRAIT_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_COSMOSAURUS_RESOURCE_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_COSMOSAURUS_TOTAL_POINTS)},
+                    {cursor.getColumnIndex(GameLogEntry.COLUMN_SCOUTARS_MISSION_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_SCOUTARS_PLAYER_BOARD_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_SCOUTARS_TRAIT_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_SCOUTARS_RESOURCE_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_SCOUTARS_TOTAL_POINTS)},
+                    {cursor.getColumnIndex(GameLogEntry.COLUMN_ARAKLITH_MISSION_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_ARAKLITH_PLAYER_BOARD_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_ARAKLITH_TRAIT_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_ARAKLITH_RESOURCE_POINTS),
+                            cursor.getColumnIndex(GameLogEntry.COLUMN_ARAKLITH_TOTAL_POINTS)}
+            };
+
+            int tuskadonTotal = mCursor.getInt(columnIndices[TUSKADON_INDEX][TOTAL_POINTS_INDEX]);
+            int starlingTotal = mCursor.getInt(columnIndices[STARLING_INDEX][TOTAL_POINTS_INDEX]);
+            int cosmosaurusTotal = mCursor.getInt(columnIndices[COSMOSAURUS_INDEX][TOTAL_POINTS_INDEX]);
+            int scoutarsTotal = mCursor.getInt(columnIndices[SCOUTAR_INDEX][TOTAL_POINTS_INDEX]);
+            int araklithTotal = mCursor.getInt(columnIndices[ARAKLITH_INDEX][TOTAL_POINTS_INDEX]);
+
+            String[] species = {getString(R.string.tuskadon),
+                    getString(R.string.starlings),
+                    getString(R.string.cosmosaurus),
+                    getString(R.string.scoutars),
+                    getString(R.string.araklith)};
+            int[] totals = {tuskadonTotal, starlingTotal, cosmosaurusTotal, scoutarsTotal, araklithTotal};
+                        int index = 0;
+            for (int pointTotal : totals) {
+                if (pointTotal != 0) {
+                    pointsColumnLayouts[index].setVisibility(View.VISIBLE);
+                    headerImages[index].setVisibility(View.VISIBLE);
+                    selectedSpecies.add(species[index]);
+
+                    TextView[] mTextViews = pointsTextViews[index];
+                    int missionPoints = mCursor.getInt(columnIndices[index][MISSION_POINTS_INDEX]);
+                    mTextViews[MISSION_POINTS_INDEX].setText(String.valueOf(missionPoints));
+                    int playerBoardPoints = mCursor.getInt(columnIndices[index][PLAYER_BOARD_POINTS_INDEX]);
+                    mTextViews[PLAYER_BOARD_POINTS_INDEX].setText(String.valueOf(playerBoardPoints));
+                    int traitPoints = mCursor.getInt(columnIndices[index][TRAIT_POINTS_INDEX]);
+                    mTextViews[TRAIT_POINTS_INDEX].setText(String.valueOf(traitPoints));
+                    int resourcesPoints = mCursor.getInt(columnIndices[index][RESOURCES_POINTS_INDEX]);
+                    mTextViews[RESOURCES_POINTS_INDEX].setText(String.valueOf(resourcesPoints));
+
+                    mTextViews[TOTAL_POINTS_INDEX].setText(String.valueOf(pointTotal));
+                } else {
+                    pointsColumnLayouts[index].setVisibility(View.GONE);
+                    headerImages[index].setVisibility(View.GONE);
+                }
+                index++;
+            }
+
+            int numSpecies = selectedSpecies.size();
+            String lastSpecies = selectedSpecies.get(numSpecies-1);
+            if (!lastSpecies.equals(getString(R.string.araklith))) {
+                endLineImages[numSpecies-1].setVisibility(View.GONE);
+            }
+
+
             mCursor.close();
         }
     }
