b4fffeab7fc68f3c077f3288ed87fd0da3882fad
fullmers <sarah.fullmer@gmail.com>
2018-01-11 10:09:02 +0100

    polish game log activity layout

--- app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/WelcomeScreenActivity.java
+++ app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/WelcomeScreenActivity.java
@@ -9,7 +9,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
