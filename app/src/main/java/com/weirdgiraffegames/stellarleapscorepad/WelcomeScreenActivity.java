c21243a345ca78ab4d0377d6c30f13090460edc5
fullmers <sarah.fullmer@gmail.com>
2018-01-11 09:13:21 +0100

    resize logo, butterknife welcome screen

--- app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/WelcomeScreenActivity.java
+++ app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/WelcomeScreenActivity.java
@@ -6,43 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class WelcomeScreenActivity extends AppCompatActivity {
 
-    Button btn_about;
-    Button btn_start_scoring;
-    Button btn_game_logs;
+    @BindView(R.id.score_new_game_btn) Button btn_start_scoring;
+    @BindView(R.id.about_app_btn) Button btn_about;
+    @BindView(R.id.game_log_btn) Button btn_game_logs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-
-        btn_about = findViewById(R.id.about_app_btn);
-        btn_about.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(WelcomeScreenActivity.this,AboutActivity.class);
-                startActivity(i);
-            }
-        });
-
-        btn_start_scoring = findViewById(R.id.score_new_game_btn);
-        btn_start_scoring.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(WelcomeScreenActivity.this,ChooseRacesActivity.class);
-                startActivity(i);
-            }
-        });
-
-        btn_game_logs = findViewById(R.id.game_log_btn);
-        btn_game_logs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(WelcomeScreenActivity.this,GameLogActivity.class);
-                startActivity(i);
-            }
-        });
+        ButterKnife.bind(this);
 
         if (getIntent().getBooleanExtra("EXIT", false)) {
             finish();
@@ -54,5 +32,23 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         super.onBackPressed();
         finish();
         moveTaskToBack(true); // exit app
+    }
+
+    @OnClick(R.id.score_new_game_btn)
+    public void scoreNewGame(View view) {
+        Intent i = new Intent(WelcomeScreenActivity.this,ChooseRacesActivity.class);
+        startActivity(i);
+    }
+
+    @OnClick(R.id.about_app_btn)
+    public void openAboutActivity(View view) {
+        Intent i = new Intent(WelcomeScreenActivity.this,AboutActivity.class);
+        startActivity(i);
+    }
+
+    @OnClick(R.id.game_log_btn)
+    public void seeGameLog(View view) {
+        Intent i = new Intent(WelcomeScreenActivity.this,GameLogActivity.class);
+        startActivity(i);
     }
 }
