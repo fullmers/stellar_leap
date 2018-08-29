7fa2927efa6a9486368a061a799174cc41e001de
fullmers <sarah.fullmer@gmail.com>
2018-08-29 13:45:09 +0200

    add launch icon, description of weird giraffe games

--- app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/AboutActivity.java
+++ app/src/main/java/com/weirdgiraffegames/stellarleapscorepad/AboutActivity.java
@@ -1,13 +1,32 @@
 package com.weirdgiraffegames.stellarleapscorepad;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AboutActivity extends AppCompatActivity {
+
+   // @BindView(R.id.about_stellar_leap) TextView tv_about_stellar_leap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.about_stellar_leap)
+    public void openStellarLeapPage(View view) {
+        String url = getString(R.string.stellar_leap_url);
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
